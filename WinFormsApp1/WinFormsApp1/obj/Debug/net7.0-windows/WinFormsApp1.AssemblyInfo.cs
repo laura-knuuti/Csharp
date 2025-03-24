5d3949696fde3f1b1d14cffcbbb0862885b7b9d0
laura-knuuti <laura.knuuti@gmail.com>
2025-03-24 08:02:30 +0200

    kommentti

--- WinFormsApp1/WinFormsApp1/obj/Debug/net7.0-windows/WinFormsApp1.AssemblyInfo.cs
+++ WinFormsApp1/WinFormsApp1/obj/Debug/net7.0-windows/WinFormsApp1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WinFormsApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+01e5f63b2f33ef134e14833524af81b8146a2858")]
 [assembly: System.Reflection.AssemblyProductAttribute("WinFormsApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WinFormsApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ kahvila/kahvila/Form1.Designer.cs
@@ -0,0 +1,40 @@
+﻿namespace kahvila
+{
+    partial class ruokalistaForm
+    {
+        private Panel vasenPL;
+        private Panel meistaPL;
+        private Panel ruoatPL;
+        private Button button1;
+        private Panel juomatPL;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private Panel ylaPL;
+        private Panel herkutPL;
+        private Panel koriPL;
+        private Panel panel1;
+        private Panel panel5;
+        private Button koriBT;
+        private Panel panel4;
+        private Button meistaBT;
+        private Panel panel3;
+        private Button herkutBT;
+        private Panel panel2;
+        private Button juomatBT;
+        private Label label1;
+        private PictureBox pictureBox1;
+        private Label meistaLB;
+        private Label makeatLB;
+        private Label suolaisetLB;
+        private Label label3;
+        private Label label2;
+        private Label kylmatLB;
+        private Label lampimatLB;
+        private Label label5;
+        private Label label4;
+        private Label label7;
+        private Label label6;
+        private Label muutLB;
+        private Label makeisetLB;
+        private Label label8;
+    }
+    }
