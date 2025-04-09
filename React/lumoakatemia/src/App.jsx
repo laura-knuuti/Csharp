b7653973abfab249f0ee25a172ea9660a2ebbc77
laura-knuuti <laura.knuuti@gmail.com>
2025-04-09 11:33:26 +0300

    kommentti

---
+++ React/lumoakatemia/src/App.jsx
@@ -0,0 +1,62 @@
+import React from 'react';
+import R18 from './R18';
+import R19 from './R19';
+import R20 from './R20';
+import R21 from './R21';
+import R22 from './R22';
+
+/*
+//R18
+function App() {
+  return (
+    <div className="App">
+      <R18 />
+    </div>
+  );
+}
+
+export default App;
+
+//R19
+function App() {
+  return (
+    <div className="App">
+      <R19 />
+    </div>
+  );
+}
+
+export default App;
+
+//R20
+function App() {
+  return (
+    <div className="App">
+      <R20 />
+    </div>
+  );
+}
+
+export default App;
+
+//R21
+function App() {
+  return (
+  <div className="App">
+    <R21 name = "Maailma" />
+  </div>
+  );
+}
+
+export default App;*/
+
+//R22
+function App() {
+  return (
+  <div className="App">
+    <R22 nimi = "Appelsiini" hinta = "0,30"/>
+  </div>
+  );
+}
+
+export default App;
