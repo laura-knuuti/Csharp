b80d307257d67f183b7cedb2e09e91b6526d9b52
laura.knuuti <laura.knuuti@gmail.com>
2025-04-10 13:59:31 +0300

    kommentti

---
+++ hotelli – kopio/hotelli/Varaus.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using MySql.Data.MySqlClient;
+using Eramake;
+using Google.Protobuf.WellKnownTypes;
+using System.Linq.Expressions;
+
+
+namespace hotelli
+{
+    class Varaus
+    {
+
+        Yhdista yhteys = new Yhdista();
+
+
+        //haetaan kaikki varaukset
+        public DataTable huonetyyppilista()
+        {
+            MySqlCommand komento = new MySqlCommand("SELECT = FROM huonekategoria", yhteys.OtaYhteys());
+            MySqlDataAdapter adapteri = new MySqlDataAdapter();
+            DataTable taulu = new DataTable();
+
+            adapteri.SelectCommand = komento;
+            adapteri.Fill(taulu);
+            return taulu;
+        }
+
+        public DataTable haeVaraukset()
+        {
+            MySqlCommand komento = new MySqlCommand("SELECT = FROM varaukset", yhteys.OtaYhteys());
+            MySqlDataAdapter adapteri = new MySqlDataAdapter();
+            DataTable taulu = new DataTable();
+
+            adapteri.SelectCommand = komento;
+            adapteri.Fill(taulu);
+            return taulu;
+
+        }
+
+
+        public bool lisaaVaraus(int hnro, int anro, DateTime sisaan, DateTime ulos)
+        {
+            MySqlCommand komento = new MySqlCommand();
+            String lisayskysely = "INSERT INTO `varaukset`" +
+            "(HuoneenNro, AsiakasId, Sisaan, Ulos) " +
+            "VALUES (@hno, @aid, @sis, @ulo); ";
+
+            komento.CommandText = lisayskysely;
+            komento.Connection = yhteys.OtaYhteys();
+
+            komento.Parameters.Add("@hno", MySqlDbType.Int32).Value = hnro;
+            komento.Parameters.Add("@aid", MySqlDbType.Int32).Value = anro;
+            komento.Parameters.Add("@sis", MySqlDbType.Date).Value = sisaan;
+            komento.Parameters.Add("@ulo", MySqlDbType.Date).Value = ulos;
+            yhteys.avaaYhteys();
+            try
+            {
+                if (komento.ExecuteNonQuery() == 1)
+                {
+                    yhteys.suljeYhteys();
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Virhe: " + ex);
+                return true;
+            }
+        }
+        public bool muokkaaVarausta(int hnro, int asid, DateTime sisaan, DateTime ulos, int vara)
+        {
+            MySqlCommand komento = new MySqlCommand();
+            String paivityskysely = "UPDATE `varaukset` SET `HuoneenNro` = @hnro," +
+                "`AsiakasId` = @aid, `Sisaan` = @sis, `Ulos` = @ulo" +
+             "WHERE VarausId = @vid";
+            komento.CommandText = paivityskysely;
+            komento.Connection = yhteys.OtaYhteys();
+            komento.Parameters.Add("@hnro", MySqlDbType.Int32).Value = hnro;
+            komento.Parameters.Add("@aid", MySqlDbType.Int32).Value = asid;
+            komento.Parameters.Add("@sis", MySqlDbType.Date).Value = sisaan;
+            komento.Parameters.Add("@ulo", MySqlDbType.Date).Value = ulos;
+            komento.Parameters.Add("@ulo", MySqlDbType.Int32).Value = vara;
+
+            yhteys.avaaYhteys();
+            if (komento.ExecuteNonQuery() == 1)
+            {
+                yhteys.suljeYhteys();
+                return true;
+            }
+            else
+            {
+                yhteys.suljeYhteys();
+                return false;
+            }
+        }
+        public bool poistaVaraus(String varausnro)
+        {
+            MySqlCommand komento = new MySqlCommand();
+            String poistokysely = "DELETE FROM varaukset WHERE VarausId = @vno";
+            komento.CommandText = poistokysely;
+            komento.Connection = yhteys.OtaYhteys();
+            komento.Parameters.Add("@vno", MySqlDbType.VarChar).Value = varausnro;
+
+
+            yhteys.avaaYhteys();
+            if (komento.ExecuteNonQuery() == 1)
+            {
+                yhteys.suljeYhteys();
+                return true;
+            }
+            else
+            {
+                yhteys.suljeYhteys();
+                return false;
+            }
+        }
+    }
+}
